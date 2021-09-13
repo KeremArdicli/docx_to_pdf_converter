c410bfeb1803f6f0c4ff02a5800fc548b96196ff
KeremArdicli <72523365+KeremArdicli@users.noreply.github.com>
2021-09-13 23:59:39 +0300

    Update app3.py
    First commit

--- app3.py
+++ app3.py
@@ -1,7 +1,6 @@
 from docx2pdf import convert
 import tkinter.filedialog
 
-directory_input = tkinter.filedialog.askdirectory(title='Word dosyalarının bulunduğu klasörü seçiniz!')
-directory_output = tkinter.filedialog.askdirectory(title='PDF dosylarını kaydetmek istediğiniz klasörü seçiniz!')
-
+directory_input = tkinter.filedialog.askdirectory(title='Select the directory where word files are located')
+directory_output = tkinter.filedialog.askdirectory(title='Select the directory where you want to save pdf files')
 convert(directory_input, directory_output)
